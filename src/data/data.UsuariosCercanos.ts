a6e4cfa0d47738acc2153e3f9b59e4230ab0b6a4
Finger Group <fingergroupcol@gmail.com>
2018-03-27 19:15:16 -0500

    Jason usuarios

--- src/data/data.UsuariosCercanos.ts
+++ src/data/data.UsuariosCercanos.ts
@@ -1,65 +1,94 @@
 export const USUARIOS =[{
+  usalIdEmisor:1,
+  usuaIdEmisor:1,
+  usuaNombreEmisor:"Sandra",
+  usuaCorreoEmisor: "correo@correo.com",
+  latitudEmisor: 5.3,
+  longitudEmisor: -73.35,
+  reluIdReceptor: 1,
+  usalIdReceptor:2,
   idUsuario: 1,
   nombreUsuario: "Nombre Usuario",
   correoUsuario: "correo@correo.com",
-  fechaCreacionUsuario: "01/01/2018",
-  celular: "3192566724",
-  localizacion: "POINT (5.541254, -73.358421)",
+  latitudReceptor: 5.3,
+  longitudReceptor: -73.35,
   porcentajeLlenado:85,
-  distancia:1,
+  distanciaKm:1,
   tickets: [{
 
     idTicket: 6,
     nombreTicket: "James",
     nombreTicketIngles: "James",
-    numeroTicket: 6
+    numeroTicket: 6,
+    numeroSeccion: 2
   },{
 
     idTicket: 5,
     nombreTicket: "James",
     nombreTicketIngles: "James",
-    numeroTicket: 5
+    numeroTicket: 5,
+    numeroSeccion: 2
   }]
 },
 {
+  usalIdEmisor:1,
+  usuaIdEmisor:1,
+  usuaNombreEmisor:"Sandra",
+  usuaCorreoEmisor: "correo@correo.com",
+  latitudEmisor: 5.3,
+  longitudEmisor: -73.35,
+  reluIdReceptor: 1,
+  usalIdReceptor:2,
   idUsuario: 2,
   nombreUsuario: "Sandra Rodriguez",
   correoUsuario: "correo@correo.com",
-  fechaCreacionUsuario: "01/01/2018",
-  celular: "3192566724",
-  localizacion: "POINT (5.541254, -73.358421)",
+  latitudReceptor: 5.3,
+  longitudReceptor: -73.35,
   porcentajeLlenado:100,
   distancia: 10,
   tickets: [{
     idTicket: 4,
     nombreTicket: "James",
     nombreTicketIngles: "James",
-    numeroTicket: 4
+    numeroTicket: 4,
+    numeroSeccion: 2
   },{
     idTicket: 3,
     nombreTicket: "James",
     nombreTicketIngles: "James",
-    numeroTicket: 3
+    numeroTicket: 3,
+    numeroSeccion: 2
   }]
 },
 {
+  usalIdEmisor:1,
+  usuaIdEmisor:1,
+  usuaNombreEmisor:"Sandra",
+  usuaCorreoEmisor: "correo@correo.com",
+  latitudEmisor: 5.3,
+  longitudEmisor: -73.35,
+  reluIdReceptor: 1,
+  usalIdReceptor:2,
   idUsuario: 3,
   nombreUsuario: "Dario Rincon",
   correoUsuario: "correo@correo.com",
   fechaCreacionUsuario: "01/01/2018",
   celular: "3192566724",
-  localizacion: "POINT (5.541254, -73.358421)",
+  latitudReceptor: 5.3,
+  longitudReceptor: -73.35,
   porcentajeLlenado:20,
   distancia:20,
   tickets: [{
     idTicket: 2,
     nombreTicket: "James",
     nombreTicketIngles: "James",
-    numeroTicket: 2
+    numeroTicket: 2,
+    numeroSeccion: 2
   },{
     idTicket: 1,
     nombreTicket: "James",
     nombreTicketIngles: "James",
-    numeroTicket: 1
+    numeroTicket: 1,
+    numeroSeccion: 2
   }]
 }]
